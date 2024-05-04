6650f8e5631cc8383eaa6e67ce788348d8d0bbd5
NOHP404 <poopa_somth@cmu.ac.th>
2024-05-04 15:21:51 -0400

    add assets

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import navBarVue from '@/components/NavBar.vue'
 </script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <header class="w-full p-2 bg-bb-black h-[8vh] fixed top-0 right-0 left-0">
+    <img src="/src/assets/logo-text-dark.svg" class="h-full" />
+  </header>
+  <section class="w-full h-[84vh] bg-bb-orange mt-[8vh]"></section>
+  <navBarVue></navBarVue>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bb-black': {
+          DEFAULT: '#22092C',
+          light: '#37233F'
+        },
+        'bb-maroon': '#872341',
+        'bb-red': '#BE3144',
+        'bb-orange': '#F05941'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
